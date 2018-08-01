cd0b5396e43120fa5c26430107ae79e77341d3f1
Unknown <frederickmorin18@gmail.com>
2018-08-01 13:04:20 -0400

    Implement Order component

---
+++ src/components/Order.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import styled from 'react-emotion';
+import isEmpty from 'lodash.isempty';
+import formatMoney from 'accounting-js/lib/formatMoney.js';
+import { Icon, Table } from 'semantic-ui-react';
+
+const FooterRow = styled(Table.Row)`
+  font-weight: bold;
+`;
+
+class Order extends React.Component {
+  state = {};
+  render() {
+    const { activeOrder } = this.props;
+    const rows = [];
+    let total = 0;
+    if (!isEmpty(activeOrder)) {
+      for (const [name, { price, quantity }] of Object.entries(activeOrder)) {
+        total += price * quantity;
+        rows.push(
+          <Table.Row>
+            <Table.Cell>{quantity}</Table.Cell>
+            <Table.Cell>{name}</Table.Cell>
+            <Table.Cell>{formatMoney(price * quantity)}</Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="remove circle" size="large" color="red" />
+            </Table.Cell>
+          </Table.Row>
+        );
+      }
+    }
+
+    return (
+      <Table unstackable selectable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>QTY</Table.HeaderCell>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Price</Table.HeaderCell>
+            <Table.HeaderCell />
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>{rows}</Table.Body>
+        <FooterRow>
+          <Table.Cell>Total:</Table.Cell>
+          <Table.Cell />
+          <Table.Cell>{formatMoney(total)}</Table.Cell>
+          <Table.Cell />
+        </FooterRow>
+      </Table>
+    );
+  }
+}
+
+export default connect(({ activeOrder }) => ({ activeOrder }))(Order);
